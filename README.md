fcda7379cbda39d8ddd75859f6533551dbb21c5a
Stefano Borzoni <s.borzoni@doctype.it>
2019-01-14 12:59:24 +0100

    remove rates.py

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # BRT Cap CSV Builder
 This project is a tool used to build a csv for [this](https://docs.shipperhq.com/category/troubleshooting/ecommerce-platform/matrixrates/) Magento 2 utility.
 
-The use is pretty simple, just change the rates.py file and the config.py file based on your necessity and then run main.py.
+#### How To
+The use is pretty simple, just change the __config.py__ file based on your necessity and then run main.py.
+
+___
 
 __matrixrates.ods__ is an example of output (in sheet format, not csv, as the name says).
--- config.py
+++ config.py
@@ -1,10 +1,90 @@
-from rates import *
 from settings import caps_type
 
 # this will be the output file
 csv_file = 'result.csv'
+
 # csv headers
 csv_fields = ['Country', 'Region/State', 'City', 'Zip/Postal Code From', 'Zip/Postal Code To', 'Weight>', 'Weight<=', 'Shipping Price', 'Shipping Method']
+
+# rates
+rates = {
+    'standard': [
+        {
+            'weight_min': '0',
+            'weight_max': '1',
+            'price': 4.50,
+        },
+        {
+            'weight_min': '1',
+            'weight_max': '10',
+            'price': 5.9,
+        },
+        {
+            'weight_min': '10',
+            'weight_max': '*',
+            'price': 0,
+        },
+    ],
+    'priority': [
+        {
+            'weight_min': '0',
+            'weight_max': '1',
+            'price': 6,
+        },
+        {
+            'weight_min': '1',
+            'weight_max': '10',
+            'price': 7.4,
+        },
+        {
+            'weight_min': '10',
+            'weight_max': '*',
+            'price': 2,
+        },
+    ],
+    'ten': [
+        {
+            'weight_min': '0',
+            'weight_max': '1',
+            'price': 7.50,
+        },
+        {
+            'weight_min': '1',
+            'weight_max': '10',
+            'price': 8.9,
+        },
+        {
+            'weight_min': '10',
+            'weight_max': '*',
+            'price': 4,
+        },
+    ],
+    'disadvantaged': [
+        {
+            'weight_min': '0',
+            'weight_max': '1',
+            'price': 7.50,
+        },
+        {
+            'weight_min': '1',
+            'weight_max': '10',
+            'price': 8.9,
+        },
+        {
+            'weight_min': '10',
+            'weight_max': '*',
+            'price': 0,
+        },
+    ],
+    'free_zone': [
+        {
+            'weight_min': '*',
+            'weight_max': '*',
+            'price': 40,
+        },
+    ],
+}
+
 # methods
 methods = [
     {
@@ -12,31 +92,31 @@ methods = [
         'Method': 'Standard',
         'caps_type': caps_type['ARRAY'],
         'caps_array': [23041, 22061],
-        'rates': free_zone_rates,
+        'rates': rates['free_zone'],
     },
     {
         # standard
         'Method': 'Standard',
         'caps_type': caps_type['ALL'],
-        'rates': standard_rates,
+        'rates': rates['standard'],
     },
     {
         # disadvantaged
         'Method': 'Disagiate e Is. Minori',
         'caps_type': caps_type['FILE'],
         'caps_file': 'disadvantaged.txt',
-        'rates': disadvantaged_rates,
+        'rates': rates['disadvantaged'],
     },
     {
         # priority
         'Method': 'Priority',
         'caps_type': caps_type['ALL'],
-        'rates': priority_rates,
+        'rates': rates['priority'],
     },
     {
         # 10.30
         'Method': 'Entro le 10.30',
         'caps_type': caps_type['ALL'],
-        'rates': ten_rates,
+        'rates': rates['ten'],
     },
 ]
